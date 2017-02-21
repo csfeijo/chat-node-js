9e5cbd6ba6f7e8e447ace04ae287cfa8882af186
csfeijo <cicero.feijo@gmail.com>
2017-02-21 16:05:07 -0300

    Add socket.io to the server/client - messages are only logged

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 var express = require('express'),
-    path = require('path'),
     app = express(),
-    hbs = require('express-handlebars');
+    server = require('http').createServer(app),
+    io = require('socket.io')(server),
+    hbs = require('express-handlebars'),
+    path = require('path');
 
 app.engine('handlebars', hbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.route('/welcome')
   .get(function (req, res) {
-    res.render('welcome', { name: "Cícero Feijó" });
+
+  res.render('welcome', { name: "Cícero Feijó" });
 });
 
-app.listen(8080, function() {
-  console.log('Server Running...');
-} );
+io.on('connection', function(socket){  
+  console.log('um usuario conectou');
+
+  socket.on('disconnect', function(){
+    console.log('usuario desconectou');
+  });
+
+  socket.on('chat message', function(data){  
+    console.log('message: ' + data.message);
+  });
+
+});
+
+server.listen(8080);
