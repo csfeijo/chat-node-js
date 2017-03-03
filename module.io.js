23f06c65317aab0a678ceba30c598f0eb5c400ff
csfeijo <cicero.feijo@gmail.com>
2017-03-03 13:09:31 -0300

    Add member list to UI and Server with validations

--- module.io.js
+++ module.io.js
@@ -1,23 +1,48 @@
 function Io(data) {
-  var io = data.io;
+  var io = data.io,
+      USERS = [];
 
   io.on('connection', function(socket) {  
     console.log('user connected');
 
-    io.emit('new-message', { message: 'new user logged!', nickName: 'SERVER' });
+    socket.on('add-user', function(data) {
+      var nickName = data.nickName,
+          userExist = false;
+
+      for (var index = 0; index < USERS.length; index++){
+        if (USERS[index].nickName == data.nickName){
+          userExist = true;
+        }
+      }
+
+      if(userExist === false) {
+        USERS.push({ nickName: nickName });
+      }
+    });
+
+    // TODO: the USERS isnt avaliable in the first message!!
+    io.emit('new-message', { message: 'new user logged!', nickName: 'SERVER', users: USERS });
 
     socket.on('disconnect', function() {
       console.log('user disconnected');
     });
 
-    socket.on('logout', function() {
+    socket.on('logout', function(data) {
       console.log('user logout');
+
+      // TODO: improve this implementation - add this feature when users is disconnected with another methods
+      for (var index = 0; index < USERS.length; index++){
+        if (USERS[index].nickName == data.nickName){
+          console.log('removed user', data.nickName);
+          USERS.splice(index, 1);
+        }
+      }
     });
 
     socket.on('chat-message', function(data) {  
       console.log('nickName: ' + data.nickName, 'message: ' + data.message);
 
-      io.emit('new-message', { message: data.message, nickName: data.nickName });
+      io.emit('new-message', { message: data.message, nickName: data.nickName, users: USERS });
     });
 
   });
